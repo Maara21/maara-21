12110a9283e6d31694b81e300548531da68780b3
Maara@21 <88497030+Maara21@users.noreply.github.com>
2021-08-13 12:57:35 +0530

    Initial commit

---
+++ Maara.py
@@ -0,0 +1,25 @@
+- 👋 Hi, I’m @Maara21
+- 👀 I’m interested in ...
+- 🌱 I’m currently learning ...
+- 💞️ I’m looking to collaborate on ... 
+- 📫 How to reach me ...
+
+<!---
+Maara21/Maara21 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes
+
+
+Python Code:
+
+def personal_details():
+    name, age = "maara", 21
+    address = "Bangalore, Karnataka, India"
+    print("Name: {}\nAge: {}\nAddress: {}".format(name, age, address))
+
+personal_details()
+
+Sample Output:
+
+Name: maara                                                                                                  
+Age: 21                                                                                                      
+Address:Bangalore Karnataka India 
